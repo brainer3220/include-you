4c06e10a7426dc0097f3111fa7e2fca8df4215d7
mir99712 <mir99712@naver.com>
2019-03-19 21:54:37 +0900

    스태틱으로 만듦

--- include you/include you/you.cpp
+++ include you/include you/you.cpp
@@ -1,7 +1,12 @@
 #include "you"
+
 int main() {
-	You you;
-	you.putln("Hello").putln("world!");
-	you.put("¤»", 10);
-	you.get<int>();
+	using namespace You;
+
+	console.putln("Hello").putln("world!");
+	console.put("¤»", 10);
+	int o;
+	console.get(o).put(o).get(o).put(o);
+	char buf[2][256];
+	console.get(buf[0], buf[1]).putln(buf[0]).putln(buf[1]);
 }
--- include you/include you/you.h
+++ include you/include you/you.h
@@ -1,53 +1,74 @@
 #ifndef INCLUDE_YOU_LIB
 #define INCLUDE_YOU_LIB
 #include <iostream>
-class You {
-public:
-	//Hello World!를 출력합니다.
-	void printHelloWorld() {
-		std::cout << "Hello World!";
-	}
-
-	//<Type>형으로 입력을 받아 값을 반환합니다.
-	template <class T>
-	const T& get() {
-		T input;
-		std::cin >> input;
-		return input;
-	}
-
-
-	//<Type>형의 값을 repeat번 출력합니다.
-	template <class T>
-	You& put(const T& output, size_t repeat=1) {
-		while(repeat--)
-			std::cout << output;
-		return *this;
-	}
-
-	//<Type>형의 값을 출력하고 개행합니다.
-	template <class T>
-	You& putln(const T& output) {
-		std::cout << output << "\n";
-		return *this;
-	}
-
-	//개행합니다.
-	You& putln() {
-		std::cout << "\n";
-		return *this;
-	}
-
-	/*//별을 찍습니다.
-	void putStar(int n) {
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < i; j++) {
-				put("*");
-			}
-			putln();
+namespace You {
+	class Console {
+		Console() {}
+	public:
+		static Console& getInstance() { static Console console; return console; }
+		//Hello World!를 출력합니다.
+		void printHelloWorld() {
+			std::cout << "Hello World!";
 		}
-	}*/
+
+		//<Type>형으로 입력을 받아 값을 반환합니다.
+		template <class T>
+		const T& get() {
+			T input;
+			std::cin >> input;
+			return input;
+		}
 
 
-};
+		//<Type>형의 값을 repeat번 출력합니다.
+		template <class T>
+		Console& put(const T& output, size_t repeat = 1) {
+			while (repeat--)
+				std::cout << output;
+			return *this;
+		}
+
+		//<Type>형의 값을 출력하고 개행합니다.
+		template <class T>
+		Console& putln(const T& output) {
+			std::cout << output << "\n";
+			return *this;
+		}
+
+		//개행합니다.
+		Console& putln() {
+			std::cout << "\n";
+			return *this;
+		}
+
+		template <class T>
+		Console& get(T& o)
+		{
+			std::cin >> o;
+			return *this;
+		}
+
+		template <class T, class ...Args>
+		Console& get(T& o, Args ...args) {
+			std::cin >> o;
+			return get(args...);
+		}
+
+		/*//별을 찍습니다.
+		void putStar(int n) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 0; j < i; j++) {
+					put("*");
+				}
+				putln();
+			}
+		}*/
+
+		template <class ...Char>
+		char* concatChar(char c, Char ...args) {
+
+		}
+	};
+	static Console& console = Console::getInstance();
+}
 #endif
